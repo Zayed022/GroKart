568cbcd8d5f84f736cbf2168b64dc463a0a4c595
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2025-03-04 01:33:10 +0530

    Deployment urls added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':"http://localhost:2020",
+      '/api':"https://grokart-2.onrender.com",
     },
   },
   
