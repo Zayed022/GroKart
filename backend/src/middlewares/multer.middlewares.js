d9860ded3e662045ca707415982468e70cf44b8f
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2025-05-13 20:01:33 +0530

    FIx multer issue

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -1,16 +1,18 @@
+import fs from "fs";
+import path from "path";
 import multer from "multer";
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(process.cwd(), "public", "temp");
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
     }
-  })
-  
-  export const upload=  multer({ 
-    storage,
- })
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Or use Date.now() + "-" + file.originalname for uniqueness
+  }
+});
+
+export const upload = multer({ storage });
