e70aab9c379f95dfcdde1a5c13659ae998b8db05
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2025-05-22 20:50:30 +0530

    Integrating our web to SEO

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemap } from 'vite-plugin-sitemap'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +15,41 @@ export default defineConfig({
     },
   },
   
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://grokartapp.com', // replace with your actual domain
+      routes: [
+        '/',
+        '/products',
+        '/cart',
+        '/about',
+        '/contact',
+        '/register',
+        '/login',
+        '/category/:categoryName',
+        '/subCategory/:subCategory',
+        '/minicategory/:miniCategory',
+        '/all-products',
+        '/products/:productId',
+        '/address',
+        '/checkout',
+        '/payment',
+        '/payment-success',
+        '/order-history',
+        '/mini',
+        '/category',
+        '/contact-us',
+        '/terms-conditions',
+        '/cancellation',
+        '/policy',
+        '/about',
+        '/offers'
+
+        // Add other important routes
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
