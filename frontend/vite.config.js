e0323153c79e70800a15a3dbaf2de2289a1a0f34
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2025-05-22 21:43:00 +0530

    Working on SEO

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemap } from 'vite-plugin-sitemap'
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
+  server: {
+    proxy: {
       '/api': {
         target: "https://grokart-2.onrender.com",
         changeOrigin: true,
@@ -14,41 +12,8 @@ export default defineConfig({
       }
     },
   },
-  
   plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://grokartapp.com', // replace with your actual domain
-      routes: [
-        '/',
-        '/products',
-        '/cart',
-        '/about',
-        '/contact',
-        '/register',
-        '/login',
-        '/category/:categoryName',
-        '/subCategory/:subCategory',
-        '/minicategory/:miniCategory',
-        '/all-products',
-        '/products/:productId',
-        '/address',
-        '/checkout',
-        '/payment',
-        '/payment-success',
-        '/order-history',
-        '/mini',
-        '/category',
-        '/contact-us',
-        '/terms-conditions',
-        '/cancellation',
-        '/policy',
-        '/about',
-        '/offers'
-
-        // Add other important routes
-      ]
-    })
+    react()
   ],
   build: {
     rollupOptions: {
@@ -60,6 +25,6 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Increases the warning limit
+    chunkSizeWarningLimit: 1000,
   },
 })
